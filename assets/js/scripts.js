15641a89062dbd7844a04b08e48a82b78cd42528
sakaoluwasola <113036798+sakaoluwasola@users.noreply.github.com>
2023-08-25 11:38:12 +0100

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+// const { easing } = require("@material-ui/core");
+
+AOS.init({
+    offset: 120,
+    delay: 900,
+    easing: 'ease',
+    once: false,
+    mirror: false,
+    anchorPlacement: 'top-bottom',
+});
